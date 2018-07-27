d07e66788f94acf114f8431f14bec4b2722fac6a
Yuvaraj Jayavel <jyuvaraj03@gmail.com>
2018-07-28 04:32:54 +0530

    First commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,77 @@ checkSetup();
 console.log(firebase);
 
 
-//computation
+//entrance json
+var entrance_ref = firebase.database().ref("entrance/");
 
-var data = "Great";
- var qrcode = new QRCode("qrcode");
+var fee = 50;
+var section = 'a'; 
+$("button").click(function() {
+  var rfid = $("#rfid-info").val();
+  // console.log(rfid);
+  
+  
+  entrance_ref.push({
+    'rfid': rfid,
+    'section': section,
+    'sno': 1,
+    'fee': 45
+  }
+);
+  section = 'b';
+})
 
- function makeCode() {
+//database config and read
+var ref = firebase.database().ref("entrance/");
+
+
+var qrcode = new QRCode("qrcode");
+
+function makeCode(data) {
 
      qrcode.makeCode(data);
  }
- makeCode();
+
+
+var foo = "BS";
+
+/*ref.once('value').then(function(snapshot) {
+  console.log(snapshot.val()['user1']);
+  section_ref = firebase.database.ref('entrance/' + snapshot.val() + '/section');
+  section_ref.once('child_added').then(function(ss) {
+
+  })
+  make
+  var section = snapshot.val().section;
+  console.log(section);
+  var fee = snapshot.val().fee;
+  console.log(fee);
+  makeCode(section + ' ' + fee);
+  // ...
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});*/
+
+// ref.orderByChild('rfid').equalTo('jyuvaraj').once('value').then(function(snapshot) {
+//   console.log(snapshot.val());
+// })
+
+entrance_ref.on('child_added',function(snapshot) {
+  // console.log(snapshot);
+  var section = snapshot.val().section;
+  // console.log(section);
+  var fee = snapshot.val().fee;
+  // console.log(fee);
+  makeCode(section + ' ' + fee);
+});
+
+
+
+//computation
+
+  // console.log(data);
+ console.log("Hello world");
+
  $("#text").
  on("blur", function() {
      makeCode();
@@ -30,3 +91,4 @@ var data = "Great";
          makeCode();
      }
  });
+
